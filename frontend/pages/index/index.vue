e5e32c37c1d5e35c4a73e610dc48ea7f356e3562
linwenbo-code <164396438@qq.com>
2021-03-08 16:39:52 +0800

    finished search engine phase 1

---
+++ frontend/pages/index/index.vue
@@ -0,0 +1,134 @@
+<template>
+	<view class="">
+		<u-form :model="form" ref="uForm">
+			<u-form-item label="搜索" prop="query">
+				<u-input v-model="form.query" />
+			</u-form-item>
+		</u-form>
+		<u-button @click="submit">搜索</u-button>
+		
+		<!--
+		<u-index-list :scrollTop="scrollTop">
+			<view v-for="item in resultList" :key="index">
+				<u-index-anchor :index="item['信息提供方']" />
+				<view class="list-cell">
+					{{ JSON.stringify(item) }}
+				</view>
+			</view>
+		</u-index-list>
+		-->
+		
+		<ol>
+			<li v-for="(item, index) in resultList">
+				<u-divider color="#fa3534">第 {{ index+1 }} 条记录</u-divider>
+				{{ JSON.stringify(item) }}
+			</li>
+		</ol>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				form: {
+					query: ''
+				},
+				rules: {
+					query: [
+						{ 
+							required: true, 
+							message: '请输入搜索内容', 
+							// 可以单个或者同时写两个触发验证方式 
+							trigger: ['change','blur'],
+						}
+					]
+				},
+				scrollTop: 0,
+				resultList: []
+			}
+		},
+		onPageScroll(e) {
+			this.scrollTop = e.scrollTop;
+		},
+		onLoad() {
+			
+		},
+		methods: {
+			submit() {
+				this.$refs.uForm.validate(valid => {
+					if (valid) {
+						console.log('验证通过');
+					} else {
+						console.log('验证失败');
+					}
+					console.log('搜索内容：' + this.form.query)
+					uni.request({
+					    url: 'http://localhost:8888', 
+						method: 'POST',
+						/*
+						header: {
+							'Content-Type': 'application/json'
+						},
+						*/
+					    data: this.form.query,
+					    success: (res) => {
+					        console.log(res.data);
+							console.log('round trip completed. ')
+							console.log('type of ' + typeof(JSON.stringify(res.data)))
+							console.log(JSON.stringify(res.data))
+							this.resultList = res.data;
+						},
+						fail: function(res) {
+							console.log(res);
+						}
+					});
+				});
+			}
+		},
+		// 必须要在onReady生命周期，因为onLoad生命周期组件可能尚未创建完毕
+		onReady() {
+			this.$refs.uForm.setRules(this.rules);
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+	
+	.list-cell {
+		display: flex;
+		box-sizing: border-box;
+		width: 100%;
+		padding: 10px 24rpx;
+		overflow: hidden;
+		color: #323233;
+		font-size: 14px;
+		line-height: 24px;
+		background-color: #fff;
+	}
+</style>
--- py_es.py
+++ py_es.py
@@ -5,13 +5,16 @@ import pandas as pd
 import json
 
 # when sheet_name=None, return a dict of dataframe
-df_dict = pd.read_excel('/Users/wenbo/Desktop/test_data.xlsx', header=[0], sheet_name=None)
+# read and parse excel
+df_dict = pd.read_excel('/Users/wenbolin/Desktop/test_data.xlsx', header=[0], sheet_name=None)
 
+# print out result to debug
 print(type(df_dict))
 print(df_dict)
 
 id = 0
 
+# feed data to elastic search at default setting localhost:9200
 for key in df_dict:
 	json_objs = df_dict[key].to_json(orient='records', force_ascii=False) #force_ascii 解决中文乱码问题
 	print (json_objs)
@@ -24,7 +27,52 @@ for key in df_dict:
 
 es.indices.refresh(index="test-data")
 
+# try a search query
 res = es.search(index="test-data", body={"query": {"query_string": {"query" : "澳门特别行政区"}}})
 print("Got %d Hits:" % res['hits']['total']['value'])
 for hit in res['hits']['hits']:
 	print(hit)
+	
+#########################################	
+#http server to receive user query
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import json
+
+data = {'result': 'this is a test'}
+host = ('localhost', 8888)
+
+class Resquest(BaseHTTPRequestHandler):
+	def do_GET(self):
+		self.send_response(200)
+		self.send_header('Content-type', 'application/json')
+		self.end_headers()
+		self.wfile.write(json.dumps(data).encode())
+	def do_POST(self):
+	
+		# process user data
+		print(self.headers)
+		print(self.command)
+		req_datas = self.rfile.read(int(self.headers['content-length'])) #重点在此步!
+		print(req_datas.decode())
+		data = {
+			'result_code': '2',
+			'result_desc': 'Success',
+			'timestamp': '',
+			'data': {'message_id': '25d55ad283aa400af464c76d713c07ad'}
+		}
+		self.send_response(200)
+		self.send_header('Content-type', 'application/json')
+		self.end_headers()
+		
+		# do search with query user typed
+		query = req_datas.decode()
+		res = es.search(index="test-data", body={"query": {"query_string": {"query" : query}}})
+		# 看看命中了多少次
+		result = "Got %d Hits:" % res['hits']['total']['value']
+		print(result)
+		self.wfile.write(json.dumps(res['hits']['hits'], ensure_ascii=False).encode('utf-8'))
+
+if __name__ == '__main__':
+    server = HTTPServer(host, Resquest)
+    print("Starting server, listen at: %s:%s" % host)
+    server.serve_forever()
